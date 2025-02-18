5c86daf2d1b494b034e80a6fb246db16fe504bfb
felipejesus-front <felipe.campos@al.resilia.com.br>
2025-02-18 19:59:01 -0300

    initiating websocket with connection and broadcast

---
+++ Dankicode-node/chat-with-websockets/index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  //   io.emit("conectado", "Estou Conectado.");
+
+  socket.broadcast.emit("usuario conectado", "Novo usuário conectado");
+
+  socket.on("disconnect", () => {
+    console.log("Usuário desconectado");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
